d2faedc31cf3006c64c33deac1f143e120c0ed0c
waraidev <wileye92@gmail.com>
2020-08-04 17:51:34 -0400

    Fixed things while writing report

--- CommandInjection/backupSweep.py
+++ CommandInjection/backupSweep.py
@@ -1,16 +1,22 @@
 import os
-import subprocess
-from shutil import make_archive, move
+from shutil import make_archive, move, rmtree
 
 print("Zip Clean Up Tool: Which folder do you want to zip?")
 path = input("Type in full folder path:")
 folder = path.split('/')[-1]
 
-if os.path.exists(path):
+# booleans to determine correct input
+no_command = not path.__contains__(';')
+no_admin = path.split('/')[1] == "Users"
+is_filepath = len(path.split(' ')) == 1 and os.path.exists(path)
+
+if no_admin and is_filepath and no_command:
     make_archive(folder, 'zip', path)
     move(os.getcwd() + "/%s.zip" % folder, path.replace(folder, "") + "/%s.zip" % folder)
-    subprocess.call(['rm', '-r', path])
+    rmtree(path)
 
     print("\n%s Folder has been zipped!\nOriginal folder has been deleted!" % folder)
+elif not no_admin:
+    print("Must be a user folder")
 else:
-    print("Path does not exist!")
+    print("Path does not exist! Hint: Path must not contain any spaces")
--- CommandInjection/badBackupSweep.py
+++ CommandInjection/badBackupSweep.py
@@ -4,7 +4,6 @@ print("Zip Clean Up Tool: Which folder do you want to zip?")
 path = input("Type in full folder path:")
 zipName = input("What do you want to name your zip file?:")
 cwd = path.replace(path.split('/')[-1], "")
-print(cwd)
 
 z = subprocess.Popen("zip -r %s %s" % (zipName, path), cwd=cwd, shell=True)
 z.communicate()
@@ -12,12 +11,12 @@ z.communicate()
 r = subprocess.Popen("rm -r %s" % path, shell=True)
 r.communicate()
 
-if z.returncode is 0:
+if z.returncode == 0:
     print("%s Folder has been zipped!" % zipName.split('.')[0])
 else:
     print("Path does not exist!")
 
-if r.returncode is 0:
+if r.returncode == 0:
     print("%s Folder was deleted" % zipName.split('.')[0])
 else:
     print("Error in deleting folder")
